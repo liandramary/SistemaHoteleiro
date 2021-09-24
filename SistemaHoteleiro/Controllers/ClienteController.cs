90e08eb6e64599d4369966346c4049d5532b8340
Liandra Mary <liandra.marysouza@gmail.com>
2021-09-23 23:10:32 -0300

    crud completo de cliente

--- SistemaHoteleiro/Controllers/ClienteController.cs
+++ SistemaHoteleiro/Controllers/ClienteController.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System.Linq;
 using SistemaHoteleiro.Models;
 using SistemaHoteleiro.Data;
@@ -19,108 +18,59 @@ namespace SistemaHoteleiro.Controllers
             _context = context;
         }
 
-    //Post: /Hotel/cliente/cadastro
-    [Route("cadastro")]
+    //Post: /Hotel/cliente/create
+    [Route("create")]
     [HttpPost]
-
     public IActionResult Create([FromBody] Cliente cliente)
     {
-            _context.Cliente.Add(cliente);
+            _context.Clientes.Add(cliente);
             _context.SaveChanges();
             return Created("", cliente);
     }
 
     //Get: /Hotel/cliente/delete
-    [Route("delete/{Cpf}")]
+    [Route("delete/{name}")]
     [HttpDelete]
-
-    public IActionResult Delete(int Cpf)
+    public IActionResult Delete(string name)
     {
-        Cliente cliente = _context.Cliente  .FirstOrDefault (cliente => cliente.Cpf == Cpf);
-=======
-using System;
-using Client.Models;
-using Hotel.Data;
-
-namespace Cliente.Controllers
-{
-    private readonly DataContext _context;
-
-    public ClienteController(DataContext context)
-    {
-        _context = context;
-    }
-
-
-    //Post: /Hotel/cliente/cadastro
-    [Route("cadastro")]
-    [httpPost]
-
-    public IActionResult Create([FromBody] Cliente cliente)
-    {
-        _context.Cliente.Add(cliente);
-        _context.SaveChanges();
-        return Created("Cliente cadastrado com sucesso", cliente);
-    }
-
-    //Get: /Hotel/cliente/delete
-    [httpDelete]
-    [Route("delete/{Cpf}")]
-
-    public IActionResult Delete(int Cpf)
-    {
-        Cliente cliente = _context.Cliente.FirstOrDefault (cliente => cliente.Cpf == Cpf);
->>>>>>> Stashed changes
+        Cliente cliente = _context.Clientes.FirstOrDefault(cliente => cliente.Nome == name);
         if (cliente == null)
         {
             return NotFound("O cliente não existe em nosso banco de dados. Verifique se preencheu os campos corretamente");
 
         }
-        _context.Cliente.Remove(cliente);
+        _context.Clientes.Remove(cliente);
         _context.SaveChanges();
-<<<<<<< Updated upstream
-        return Ok(cliente);
+        return Ok(_context.Clientes.ToList());
     }
 
-    //Put: /Hotel/cliente/alterar
-    [Route("alterar")]
-=======
-        return (Cliente);
-    }
-
-    //Put: /Hotel/cliente/alterar
+    //Put: /Hotel/cliente/update
     [Route("update")]
->>>>>>> Stashed changes
     [HttpPut]
-
     public IActionResult Update([FromBody] Cliente cliente)
     {
-        _context.Cliente.Update(cliente);
-<<<<<<< Updated upstream
+        _context.Clientes.Update(cliente);
         _context.SaveChanges();
         return Ok(cliente);
     }
 
-    //GET: /hotel/cliente/consulta
-    //[Route("consulta")]
-    //[HttpGet]
-    //public IActionResult Read() => Ok(_context.Cliente.Read());
-
-
-    //POST: /hotel/cliente/lista
-    [Route("Lista")]
+    //GET: /hotel/cliente/getbyid/2
+    [Route("getbyid/{id}")]
     [HttpGet]
-    public IActionResult List() => Ok(_context.Cliente.ToList());
-  }
-=======
-        _content.SaveChanges();
+    public IActionResult GetById([FromRoute] int id)
+    {
+        //Buscar um objeto pela chave primária
+        Cliente cliente = _context.Clientes.Find(id);
+        if (cliente == null)
+        {
+            return NotFound();
+        }
         return Ok(cliente);
     }
 
-    //Post: /Hotel/cliente/ConsultarDados
-    public IActionResult ConsultarDados([FromBody] Cliente cliente)
-    {
-        return (cliente);
-    }
->>>>>>> Stashed changes
+    //POST: /hotel/cliente/list
+    [Route("list")]
+    [HttpGet]
+    public IActionResult List() => Ok(_context.Clientes.ToList());
+  }
 }
